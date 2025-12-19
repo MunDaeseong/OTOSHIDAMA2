e2474442faa5fa08d44e6f42fcbcdd01d308ae69
desung <desung.mun@icloud.com>
2025-12-20 01:42:35 +0900

    change gift button link

--- OtoshiDama.js
+++ OtoshiDama.js
@@ -1,5 +1,14 @@
-document.getElementById("giftBtn").addEventListener("click", function () {
-    console.log("button clicked");
-    window.location.href =
-      "https://gift.starbucks.co.jp/c/1766TGSpz2mxhEMSXDO1lW7rv3nOHJAk";
+document.addEventListener("DOMContentLoaded", function () {
+    const btn = document.getElementById("giftBtn");
+  
+    if (!btn) {
+      console.error("giftBtn not found");
+      return;
+    }
+  
+    btn.addEventListener("click", function () {
+      console.log("button clicked");
+      window.location.href =
+        "https://gift.starbucks.co.jp/c/1766TGSpz2mxhEMSXDO1lW7rv3nOHJAk";
+    });
   });
