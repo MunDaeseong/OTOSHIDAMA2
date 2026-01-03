80e8c142877a4419df8e15c82085e249cffa18e5
desung <desung.mun@icloud.com>
2026-01-03 22:04:06 +0900

    change gift button link

--- OtoshiDama.js
+++ OtoshiDama.js
@@ -1,15 +1,4 @@
-const CORRECT = "0817";
 const giftURL = "https://gift.starbucks.co.jp/"; // 本物に差し替え
-
-function check() {
-  const v = document.getElementById("password").value.trim();
-  if (v === CORRECT) {
-    document.getElementById("gate").classList.add("hidden");
-    document.getElementById("card").classList.remove("hidden");
-  } else {
-    alert("ちがうみたい…");
-  }
-}
 
 // HTMLが読み込まれてから実行
 document.addEventListener("DOMContentLoaded", () => {
